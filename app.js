fd7e8c5b556c83cddbcca1a5991895c7b96a271c
AndresFeGoHe <andres.gomez8808@gmail.com>
2023-05-09 15:12:08 -0500

    cambio

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let b = "Felipe";
 console.log("hola soy " + b + " como estas " + a);
 console.log("Hola donde estas");
 
-for (let i = 0; i < 20; i ++) {
+for (let i = 0; i < 10; i ++) {
     if (i == 4)
         console.log("holi");
         console.log (i);
