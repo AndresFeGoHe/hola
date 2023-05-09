47f9cd65408917a78d947fae20c7313481a8297d
AndresFeGoHe <andres.gomez8808@gmail.com>
2023-05-09 13:30:39 -0500

    agregue for

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 let a = "Andres";
 let b = "Felipe";
-console.log("hola soy " + b +  " como estas " + a);
+console.log("hola soy " + b + " como estas " + a);
 console.log("Hola donde estas");
+
+let i = 0;
+for (let i = 0; i < 20; i ++) {
+    if (i == 4)
+        console.log("holi");
+        console.log (i);
+}
