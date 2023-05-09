c6d4aa1551b4dfc7c344d86b70cf4c4558b007af
AndresFeGoHe <andres.gomez8808@gmail.com>
2023-05-09 13:36:06 -0500

    se quita variable

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let b = "Felipe";
 console.log("hola soy " + b + " como estas " + a);
 console.log("Hola donde estas");
 
-let i = 0;
 for (let i = 0; i < 20; i ++) {
     if (i == 4)
         console.log("holi");
